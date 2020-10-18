c86533b33d33f6f0d11fb5e4e392f2b56ff217a7
Phong Phan <phan0001@risk.regn.net>
2020-10-18 16:12:42 +0700

    init project

---
+++ src/main/java/com/phonpv/edegsvc/EdegSvcApplication.java
@@ -0,0 +1,13 @@
+package com.phonpv.edegsvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdegSvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdegSvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phonpv/edegsvc/EdegSvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phonpv.edegsvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdegSvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
